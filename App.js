d260abc6661402c94038b5e64032805fef5859b6
danmeyadi <danmeyadie@gmail.com>
2025-07-05 21:16:57 +0100

    Update App.js

--- App.js
+++ App.js
@@ -1,41 +1,6 @@
-<<import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { Ionicons } from '@expo/vector-icons';
-
-// Import your screens (create these files first!)
+// Daidai imports
 import HomeScreen from './screens/HomeScreen';
 import ExploreScreen from './screens/ExploreScreen';
+import NotificationScreen from './screens/NotificationScreen'; // Tabbatar cewa sunan daidai yake
 import ProfileScreen from './screens/ProfileScreen';
-
-const Tab = createBottomTabNavigator();
-
-export default function App() {
-  return (
-    <NavigationContainer>
-      <Tab.Navigator
-        screenOptions={({ route }) => ({
-          tabBarIcon: ({ focused, color, size }) => {
-            let iconName;
-            if (route.name === 'Home') {
-              iconName = focused ? 'home' : 'home-outline';
-            }
-            else if (route.name === 'Explore') {
-              iconName = focused ? 'search' : 'search-outline';
-            }
-            else if (route.name === 'Profile') {
-              iconName = focused ? 'person' : 'person-outline';
-            }
-            return <Ionicons name={iconName} size={size} color={color} />;
-          },
-          tabBarActiveTintColor: 'blue',
-          tabBarInactiveTintColor: 'gray',
-        })}
-      >
-        <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Explore" component={ExploreScreen} />
-        <Tab.Screen name="Profile" component={ProfileScreen} />
-      </Tab.Navigator>
-    </NavigationContainer>
-  );
-}
+import LoginScreen from './screens/LoginScreen';
