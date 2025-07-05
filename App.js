1c39d326a4948fe6f3b196a4877d014eaebeef37
danmeyadi <danmeyadie@gmail.com>
2025-07-05 21:27:40 +0100

    Update App.js

--- App.js
+++ App.js
@@ -1,6 +1,3 @@
-// Daidai imports
 import HomeScreen from './screens/HomeScreen';
 import ExploreScreen from './screens/ExploreScreen';
-import NotificationScreen from './screens/NotificationScreen'; // Tabbatar cewa sunan daidai yake
-import ProfileScreen from './screens/ProfileScreen';
-import LoginScreen from './screens/LoginScreen';
+// ... repeat for others
